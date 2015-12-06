Update MCTS algorithm to backprop to the root. Update best method to choose child with highest UCT value. Update visualization to include all trial trees, and correct the seed.

experiments.py
@@ -36,9 +36,6 @@ names = ['x_mcts_vs_o_random_1',
 
 
 def run_experiment(player_policies, experiment_name, n):
-    # For reproducibility
-    np.random.seed(0)
-
     games = []
     winners = []
     X_wins = np.zeros(n)
@@ -72,11 +69,6 @@ def run_experiment(player_policies, experiment_name, n):
 
     from matplotlib import pyplot as plt
 
-    # Plot cumulative win count over time
-    # plt.plot(X_wins)
-    # plt.plot(O_wins)
-    # plt.legend(['X wins', 'O wins'])
-
     # Plot cumulative win rate over time
     plt.plot(X_win_rate)
     plt.plot(O_win_rate)
@@ -96,29 +88,20 @@ def run_experiment(player_policies, experiment_name, n):
         if type(policy) is MCTSPolicy:
             visualize_mcts_tree(mcts=policy,
                                 depth=0,
-                                filename='{}_{}'.format(name, policy.player))
+                                filename='{}_{}_{}'.format(name, policy.player, n))
 
-    # # Analyze the opening moves (todo)
-    # policy = player_policies[0]
-    # from gamestate import GameState
-    # opening_moves = policy.digraph.successors(GameState())
-    #
-    # for node in opening_moves:
-    #     print('=======================')
-    #     print(node)
-    #     print('{} / {} = {}'.format(policy.digraph.node[node]['w'],
-    #                                 policy.digraph.node[node]['n'],
-    #                                 policy.digraph.node[node]['w'] / policy.digraph.node[node]['n']))
-    #     print()
 
-    # Generate visual game trees
-    # dot_graph_combined = nx.compose_all(games)
-    # dot_graph = nx.to_pydot(dot_graph_combined)
-    # dot_graph.set_graph_defaults(fontname='Courier')
-    # dot_graph.write_png('multiple_game_graph_mcts_vs_random.png')
-
+seed = 0
 for experiment, name in zip(experiments, names):
+    # For reproducibility, we seed the PRNG
+    # Note: we use one seed per experiment, so that the different length runs start at the same seed
+    # We also change the seed for each experiment, so that we can run multiple tries of an experiment
+    # and have them produce different results
+    np.random.seed(seed)
+
     # Different number of games to play per experiment
     n = [10, 20, 50, 100, 150, 200]
     for simulation_time in n:
         run_experiment(experiment, name, simulation_time)
+
+    seed += 1

policies.py
@@ -24,7 +24,10 @@ class Policy(object):
 class RandomPolicy(Policy):
     def move(self, state):
         """Chooses moves randomly from the legal moves in a given state"""
-        return random.choice(state.legal_moves())
+        legal_moves = state.legal_moves()
+        idx = np.random.randint(len(legal_moves))
+        return legal_moves[idx]
+        # return np.random.choice(state.legal_moves())
 
 
 class MCTSPolicy(Policy):
@@ -56,31 +59,73 @@ class MCTSPolicy(Policy):
         # Constant parameter to weight exploration vs. exploitation for UCT
         self.uct_c = np.sqrt(2)
 
-        self.root = None
+        self.node_counter = 0
+
+        empty_board = GameState()
+        self.digraph.add_node(self.node_counter, attr_dict={'w': 0,
+                                                            'n': 0,
+                                                            'uct': 0,
+                                                            'expanded': False,
+                                                            'state': empty_board})
+        empty_board_node_id = self.node_counter
+        self.node_counter += 1
+
+        self.last_move = None
 
         if player is 'O':
-            # We need the empty board and its children to be present in the digraph
-            # even if the player is 'O', so that the overall root will be present
-            empty_board = GameState()
-            self.digraph.add_node(empty_board, attr_dict={'w': 0, 'n': 0, 'uct': 0, 'expanded': False})
             for successor in [empty_board.transition_function(*move) for move in empty_board.legal_moves()]:
-                self.digraph.add_node(successor, attr_dict={'w': 0, 'n': 0, 'uct': 0, 'expanded': False})
-                self.digraph.add_edge(empty_board, successor)
+                self.digraph.add_node(self.node_counter, attr_dict={'w': 0,
+                                                                    'n': 0,
+                                                                    'uct': 0,
+                                                                    'expanded': False,
+                                                                    'state': successor})
+                self.digraph.add_edge(empty_board_node_id, self.node_counter)
+                self.node_counter += 1
 
-    def move(self, root):
+    def reset_game(self):
+        self.last_move = None
+
+    def move(self, starting_state):
         # Make a copy of the starting state so that the MCTS state can't be
         # modified later from the outside
-        root = copy.deepcopy(root)
-        self.root = root
+        starting_state = copy.deepcopy(starting_state)
+        # todo: is that copy needed?
+
+        starting_node = None
+
+        if self.last_move is not None:
+            # Check if the starting state is already in the graph as a child of the last move that we made
+            exists = False
+            for child in self.digraph.successors(self.last_move):
+                # Check if the child has the same state attribute as the starting state
+                if self.digraph.node[child]['state'] == starting_state:
+                    # If it does, then check if there is a link between the last move and this child state
+                    if self.digraph.has_edge(self.last_move, child):
+                        exists = True
+                        starting_node = child
+            if not exists:
+                # If it wasn't found, then add the starting state and an edge to it from the last move
+                self.digraph.add_node(self.node_counter,
+                                      attr_dict={'w': 0,
+                                                 'n': 0,
+                                                 'uct': 0,
+                                                 'expanded': False,
+                                                 'state': starting_state})
+                self.digraph.add_edge(self.last_move,
+                                      self.node_counter)
+                starting_node = self.node_counter
+                self.node_counter += 1
+        else:
+            for node in self.digraph.nodes():
+                if self.digraph.node[node]['state'] == starting_state:
+                    starting_node = node
 
         computational_budget = 25
         for i in range(computational_budget):
             self.num_simulations += 1
 
-            print("Running MCTS from this starting state:\n{}".format(root))
-
-            # Keep track of the path so that we can do backpropagation later
-            path = nx.DiGraph()
+            print("Running MCTS from this starting state with node id {}:\n{}".format(starting_node,
+                                                                                      starting_state))
 
             # Until computational budget runs out, run simulated trials
             # through the tree:
@@ -88,19 +133,18 @@ class MCTSPolicy(Policy):
             # Selection: Recursively pick the best node that maximizes UCT
             # until reaching an unvisited node
             print('================ ( selection ) ================')
-            selected_node = self.selection(self.root, path)
-            print('selected:\n{}'.format(selected_node))
+            selected_node = self.selection(starting_node)
+            print('selected:\n{}'.format(self.digraph.node[selected_node]['state']))
 
             # Check if the selected node is a terminal state, and if so, this
             # iteration is finished
-            if selected_node.winner():
+            if self.digraph.node[selected_node]['state'].winner():
                 break
 
             # Expansion: Add a child node where simulation will start
             print('================ ( expansion ) ================')
             new_child_node = self.expansion(selected_node)
             print('Node chosen for expansion:\n{}'.format(new_child_node))
-            path.add_edge(selected_node, new_child_node)
 
             # Simulation: Conduct a light playout
             print('================ ( simulation ) ================')
@@ -109,30 +153,50 @@ class MCTSPolicy(Policy):
 
             # Backpropagation: Update the nodes on the path with the simulation results
             print('================ ( backpropagation ) ================')
-            self.backpropagation(path, new_child_node, reward)
+            self.backpropagation(new_child_node, reward)
 
-        move = self.best(root)
+        move, resulting_node = self.best(starting_node)
         print('MCTS complete. Suggesting move: {}\n'.format(move))
+
+        self.last_move = resulting_node
+
+        # If we won, reset the last move to None for future games
+        if self.digraph.node[resulting_node]['state'].winner():
+            self.last_move = None
+
         return move
 
     def best(self, root):
         """
-        Returns the action that results in the child with the most number of visits
+        Returns the action that results in the child with the highest UCT value
+        (An alternative strategy could also be used, where the action leading to
+        the child with the most number of visits is chosen
         """
         # Todo: explore various strategies for choosing the best action
         children = self.digraph.successors(root)
-        num_visits = {}
+
+        # # Option 1: Choose the child with the highest 'n' value
+        # num_visits = {}
+        # for child_node in children:
+        #     num_visits[child_node] = self.digraph.node[child_node]['n']
+        # best_child = max(num_visits.items(), key=operator.itemgetter(1))[0]
+
+        # Option 2: Choose the child with the highest UCT value
+        uct_values = {}
         for child_node in children:
-            num_visits[child_node] = self.digraph.node[child_node]['n']
+            uct_values[child_node] = self.uct(child_node)
 
         # Choose the child node that maximizes the expected value given by UCT
-        best_child = max(num_visits.items(), key=operator.itemgetter(1))[0]
+        # If more than one has the same UCT value then break ties randomly
+        best_children = [key for key, val in uct_values.iteritems() if val == max(uct_values.values())]
+        idx = np.random.randint(len(best_children))
+        best_child = best_children[idx]
 
         # Determine which action leads to this child
         action = self.digraph.get_edge_data(root, best_child)['action']
-        return action
+        return action, best_child
 
-    def selection(self, root, path):
+    def selection(self, root):
         """
         Starting at root, recursively select the best node that maximizes UCT
         until a node is reached that has no explored children
@@ -142,7 +206,13 @@ class MCTSPolicy(Policy):
         """
         # In the case that the root node is not in the graph, add it
         if root not in self.digraph.nodes():
-            self.digraph.add_node(root, attr_dict={'w': 0, 'n': 0, 'uct': 0, 'expanded': False})
+            self.digraph.add_node(self.node_counter,
+                                  attr_dict={'w': 0,
+                                             'n': 0,
+                                             'uct': 0,
+                                             'expanded': False,
+                                             'state': root})
+            self.node_counter += 1
             return root
         elif not self.digraph.node[root]['expanded']:
             print('root in digraph but not expanded')
@@ -158,38 +228,59 @@ class MCTSPolicy(Policy):
             # Choose the child node that maximizes the expected value given by UCT
             best_child_node = max(uct_values.items(), key=operator.itemgetter(1))[0]
 
-            selected_node = best_child_node
-            path.add_edge(root, selected_node)
-
-            return self.selection(selected_node, path)
+            return self.selection(best_child_node)
 
     def expansion(self, node):
         # As long as this node has at least one unvisited child, choose a legal move
         children = self.digraph.successors(node)
-        legal_moves = node.legal_moves()
+        legal_moves = self.digraph.node[node]['state'].legal_moves()
         print('Legal moves: {}'.format(legal_moves))
 
         # Select the next unvisited child with uniform probability
         unvisited_children = []
         corresponding_actions = []
+        print("legal moves: {}".format(legal_moves))
         for move in legal_moves:
-            child = node.transition_function(*move)
-            if child not in children:
+            print('adding to expansion analysis with: {}'.format(move))
+            child = self.digraph.node[node]['state'].transition_function(*move)
+
+            in_children = False
+            for child_node in children:
+                if self.digraph.node[child_node]['state'] == child:
+                    in_children = True
+
+            if not in_children:
                 unvisited_children.append(child)
                 corresponding_actions.append(move)
         # Todo: why is it possible for there to be no unvisited children?
+        print('unvisited children: {}'.format(len(unvisited_children)))
         if len(unvisited_children) > 0:
             idx = np.random.randint(len(unvisited_children))
             child, move = unvisited_children[idx], corresponding_actions[idx]
 
-            self.digraph.add_node(child, attr_dict={'w': 0, 'n': 0, 'uct': 0, 'expanded': False})
-            self.digraph.add_edge(node, child, attr_dict={'action': move})
+            self.digraph.add_node(self.node_counter,
+                                  attr_dict={'w': 0,
+                                             'n': 0,
+                                             'uct': 0,
+                                             'expanded': False,
+                                             'state': child})
+            self.digraph.add_edge(node, self.node_counter, attr_dict={'action': move})
+            child_node_id = self.node_counter
+            self.node_counter += 1
+        else:
+            # Todo:
+            # Is this the correct behavior? The issue is, it was getting to the expansion
+            # expansion method with nodes that were already expanded for an unknown reason,
+            # so here we return the node that was passed. Maybe there is a case where a
+            # node had been expanded but not yet marked as expanded until it got here.
+            return node
 
         # If all legal moves are now children, mark this node as expanded.
         if len(children) + 1 == len(legal_moves):
             self.digraph.node[node]['expanded'] = True
+            print('node is expanded')
 
-        return child
+        return child_node_id
 
     def simulation(self, node):
         """
@@ -197,16 +288,17 @@ class MCTSPolicy(Policy):
         :return: The reward obtained once a terminal state is reached
         """
         random_policy = RandomPolicy()
-        while not node.winner():
-            move = random_policy.move(node)
-            node = node.transition_function(*move)
+        current_state = self.digraph.node[node]['state']
+        while not current_state.winner():
+            move = random_policy.move(current_state)
+            current_state = current_state.transition_function(*move)
 
-        if node.winner() == self.player:
+        if current_state.winner() == self.player:
             return 1
         else:
             return 0
 
-    def backpropagation(self, path, last_visited, reward):
+    def backpropagation(self, last_visited, reward):
         """
         Walk the path upwards to the root, incrementing the
         'n' and 'w' attributes of the nodes along the way
@@ -218,11 +310,19 @@ class MCTSPolicy(Policy):
 
             print('Updating to n={} and w={}:\n{}'.format(self.digraph.node[current]['n'],
                                                           self.digraph.node[current]['w'],
-                                                          current))
+                                                          self.digraph.node[current]['state']))
 
-            # Will throw an IndexError when we arrive at the root of the path
+            # Terminate when we reach the empty board
+            if self.digraph.node[current]['state'] == GameState():
+                break
+            # Todo:
+            # Does this handle the necessary termination conditions for both 'X' and 'O'?
+            # As far as we can tell, it does
+
+            # Will throw an IndexError when we arrive at a node with no predecessors
+            # Todo: see if this additional check is no longer necessary
             try:
-                current = path.predecessors(current)[0]
+                current = self.digraph.predecessors(current)[0]
             except IndexError:
                 break
 
@@ -239,6 +339,7 @@ class MCTSPolicy(Policy):
 
         exploitation_value = w / (n + epsilon)
         exploration_value = c * np.sqrt(np.log(t) / (n + epsilon))
+        print('exploration_value: {}'.format(exploration_value))
 
         value = exploitation_value + exploration_value
 